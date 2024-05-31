4451963f3670cb7ca902b007a4bc4018db4b7f43
Herman Effendi <hermaneffendi0502@gmail.com>
2024-05-31 10:36:08 +0700

    fixing basic nft image

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -2,16 +2,15 @@
 pragma solidity ^0.8.19;
 
 import {BasicNft} from "../src/BasicNft.sol";
-import {Script} from "forge-std/Script.sol";
+import {Script, console} from "forge-std/Script.sol";
 import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
 
 contract MintBasicNft is Script{
     
-    string public constant PUG_URI =
-        "https://ipfs.io/ipfs/QmSsYRx3LpDAb1GZQm7zZ1AuHZjfbPkD6J7s9r41xu1mf8?filename=mydogie.png";
-
+    string public constant PUG_URI = "https://ipfs.io/ipfs/QmbhqHABY5vtTTDEsyNGgZf6gxbjRBi2S3Vki3a7hgcFKZ?filename=BasicNft.json";    
     function run() external {   
         address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment("BasicNft", block.chainid);
+        console.log("mostRecentlyDeployed", mostRecentlyDeployed);
         mintNftOnContract(mostRecentlyDeployed);
     }
 
--- src/BasicNft.sol
+++ src/BasicNft.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.8.19;
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract BasicNft is ERC721{
+    error BasicNft__TokenUriNotFound();
+
     uint256 private s_tokenCounter;
     mapping (uint256 => string) private s_tokenIdToUri;
     
@@ -18,6 +20,9 @@ contract BasicNft is ERC721{
     }
 
     function tokenURI(uint256 _tokenId) public view override returns (string memory) {
+        if(ownerOf(_tokenId) == address(0)) {
+            revert BasicNft__TokenUriNotFound();
+        }
         return s_tokenIdToUri[_tokenId];
     }
 
